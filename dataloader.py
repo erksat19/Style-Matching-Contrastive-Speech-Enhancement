452dab860b7095c9851ca3f25a91b9c6026c0905
drumpt <ssohot1@kaist.ac.kr>
2021-08-23 08:41:01 +0000

    Init

---
+++ dataloader.py
@@ -0,0 +1,14 @@
+import os
+import csv
+
+def parse_metadata(metadata_dir):
+    stt_dict = dict()
+    with open(metadata_dir) as metadata_file:
+        csv_reader = csv.reader(metadata_file)
+        next(csv_reader) # omit first line
+        for row in csv_reader: # ID, duration, wav, spk_id, wrd
+            stt_dict[row[2]] = row[4]
+
+    # for speech_dir, text in stt_dict.items():
+    #     print(f"{speech_dir} : {text}")
+    return stt_dict
